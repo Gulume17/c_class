1537c678222dc8127d0952459d9cd073e1f15fc6
Kyeongyunhui <yunhui72659147@gmail.com>
2025-01-22 16:51:27 +0900

    day 11

---
+++ c_class/포인터상수.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	
+	
+	*/
+
+
+	int x = 10, y = 20;
+	int* const PTR = &x; //상수는 const
+	printf("%d \n", *PTR);
+
+	*PTR = 30;
+	PTA = &y; // 상수라 안변한다
+ 
+	
+
+
+
+
+
+
+	return 0;
+}
